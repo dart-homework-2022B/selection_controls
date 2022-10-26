cf90467892ec3a85df908ee0f8599f4396d54ffa
ZARIFMAC <uzbzarif@gmail.com>
2022-10-26 15:10:08 +0500

    Radio

---
+++ lib/screens/controlradio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ControlRadio extends StatefulWidget {
+  const ControlRadio({Key? key}) : super(key: key);
+
+  @override
+  State<ControlRadio> createState() => _ControlRadioState();
+}
+
+class _ControlRadioState extends State<ControlRadio> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
